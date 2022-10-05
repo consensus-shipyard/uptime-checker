24e74737efa555eaf7c26fbc50a7c558ef748401
cryptoAtwill <willes.lau@protocol.ai>
2022-10-05 23:32:24 +0800

    fix checker logic

--- checker-go/uptime/checker.go
+++ checker-go/uptime/checker.go
@@ -226,14 +226,13 @@ func (u *UptimeChecker) recordMemberHealthInfo(actorID ActorID, upInfos *[]UpInf
 			val.Latency = (*upInfos)[i].latency
 			val.LastChecked = (*upInfos)[i].checkedTime
 
-			// not online, skip latency calculation
-			if !val.IsOnline {
-				continue
+			// if not online, skip latency calculation
+			if val.IsOnline {
+				// moving average calculation
+				newCount := val.LatencyCounts + 1
+				val.AvgLatency = val.AvgLatency * val.LatencyCounts / newCount + val.Latency / newCount
+				val.LatencyCounts = newCount
 			}
-			// moving average calculation
-			newCount := val.LatencyCounts + 1
-			val.AvgLatency = val.AvgLatency * val.LatencyCounts / newCount + val.Latency / newCount
-			val.LatencyCounts = newCount
 		}
 		healthInfos[addr] = val
 	}
