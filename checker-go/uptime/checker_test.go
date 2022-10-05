c9528de4332cb0bb5cc38474a5ce8b9d2489e969
cryptoAtwill <willes.lau@protocol.ai>
2022-10-05 23:33:56 +0800

    add more checker tests

--- checker-go/uptime/checker_test.go
+++ checker-go/uptime/checker_test.go
@@ -4,9 +4,9 @@ import (
     "testing"
 )
 
-func prepareUpInfos(latency uint64) *[]UpInfo {
+func prepareUpInfos(latency uint64, isOnline bool) *[]UpInfo {
 	upInfos := make([]UpInfo, 0)
-	upInfos = append(upInfos, UpInfo { isOnline: true, latency: latency, checkedTime: uint64(1000)})
+	upInfos = append(upInfos, UpInfo { isOnline: isOnline, latency: latency, checkedTime: uint64(1000)})
 	return &upInfos
 }
 
@@ -16,22 +16,22 @@ func prepareMultiAddrs(addr string) *[]MultiAddr {
 	return &addresses
 }
 
-func TestRecordMemberHealthInfot(t *testing.T) {
+func TestRecordMemberHealthInfo_allOnline(t *testing.T) {
 	u := UptimeChecker{
 		nodeAddresses: make(map[ActorID]map[MultiAddr]HealtcheckInfo),
 	}
 
 	actorId := ActorID(1000)
 
-	upInfos := prepareUpInfos(uint64(100))
+	upInfos := prepareUpInfos(uint64(100), true)
 	addresses := prepareMultiAddrs("abc")
 	u.recordMemberHealthInfo(actorId, upInfos, addresses)
 
-	upInfos = prepareUpInfos(uint64(200))
+	upInfos = prepareUpInfos(uint64(200), true)
 	addresses = prepareMultiAddrs("abc")
 	u.recordMemberHealthInfo(actorId, upInfos, addresses)
 
-	upInfos = prepareUpInfos(uint64(300))
+	upInfos = prepareUpInfos(uint64(300), true)
 	addresses = prepareMultiAddrs("abc")
 	u.recordMemberHealthInfo(actorId, upInfos, addresses)
 
@@ -44,6 +44,71 @@ func TestRecordMemberHealthInfot(t *testing.T) {
 	}
 
 	if u.nodeAddresses[actorId]["abc"].LatencyCounts != 3 {
+		t.Fatalf(`Should have latency count of 3`)
+	}
+}
+
+
+func TestRecordMemberHealthInfo_someOffline(t *testing.T) {
+	u := UptimeChecker{
+		nodeAddresses: make(map[ActorID]map[MultiAddr]HealtcheckInfo),
+	}
+
+	actorId := ActorID(1000)
+
+	upInfos := prepareUpInfos(uint64(100), true)
+	addresses := prepareMultiAddrs("abc")
+	u.recordMemberHealthInfo(actorId, upInfos, addresses)
+
+	upInfos = prepareUpInfos(uint64(200), false)
+	addresses = prepareMultiAddrs("abc")
+	u.recordMemberHealthInfo(actorId, upInfos, addresses)
+
+	upInfos = prepareUpInfos(uint64(300), true)
+	addresses = prepareMultiAddrs("abc")
+	u.recordMemberHealthInfo(actorId, upInfos, addresses)
+
+	if !u.nodeAddresses[actorId]["abc"].IsOnline {
+		t.Fatalf(`Should be online`)
+	}
+
+	if u.nodeAddresses[actorId]["abc"].AvgLatency != 200 {
+		t.Fatalf(`Should have avg latency of 200`)
+	}
+
+	if u.nodeAddresses[actorId]["abc"].LatencyCounts != 2 {
+		t.Fatalf(`Should have latency count of 3`)
+	}
+}
+
+func TestRecordMemberHealthInfo_finalOffline(t *testing.T) {
+	u := UptimeChecker{
+		nodeAddresses: make(map[ActorID]map[MultiAddr]HealtcheckInfo),
+	}
+
+	actorId := ActorID(1000)
+
+	upInfos := prepareUpInfos(uint64(100), true)
+	addresses := prepareMultiAddrs("abc")
+	u.recordMemberHealthInfo(actorId, upInfos, addresses)
+
+	upInfos = prepareUpInfos(uint64(200), true)
+	addresses = prepareMultiAddrs("abc")
+	u.recordMemberHealthInfo(actorId, upInfos, addresses)
+
+	upInfos = prepareUpInfos(uint64(0), false)
+	addresses = prepareMultiAddrs("abc")
+	u.recordMemberHealthInfo(actorId, upInfos, addresses)
+
+	if u.nodeAddresses[actorId]["abc"].IsOnline {
+		t.Fatalf(`Should be offline`)
+	}
+
+	if u.nodeAddresses[actorId]["abc"].AvgLatency != 150 {
+		t.Fatalf(`Should have avg latency of 150`)
+	}
+
+	if u.nodeAddresses[actorId]["abc"].LatencyCounts != 2 {
 		t.Fatalf(`Should have latency count of 3`)
 	}
 }
