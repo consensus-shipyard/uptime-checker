0586eda3a8be52db58fc959fee8e77d9c6e992ad
cryptoAtwill <willes.lau@protocol.ai>
2022-10-05 18:32:04 +0800

    fix test

--- checker-go/uptime/votes_test.go
+++ checker-go/uptime/votes_test.go
@@ -8,11 +8,11 @@ func TestHasVoted(t *testing.T) {
 	votes := Votes{}
 	votes.Votes = append(votes.Votes, 10)
 	
-	if votes.HasVoted(9) {
+	if voted, _ := votes.HasVoted(9); voted {
 		t.Fatalf(`Should not have voted for 9`)
 	}
 
-	if !votes.HasVoted(10) {
+	if voted, _ = votes.HasVoted(10); !voted {
 		t.Fatalf(`Should have voted for 10`)
 	}
 }
